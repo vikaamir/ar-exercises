377940d2e7a0145781a038a5a9c54406e9be1214
Victoria Amirov <vika.amir20@gmail.com>
2023-10-22 14:21:06 -0700

    done whit exercise6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,31 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ApplicationRecord
+  has_many :employees
+  
+end
+
+class Employee < ApplicationRecord
+  belongs_to :store
+end
+
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60
+)
+
+
+@store1.employees.create(
+  first_name: "Berdem",
+  last_name: "Doe",
+  hourly_rate: 45
+)
+
+# Creating employees for @store2 (Richmond)
+@store2.employees.create(
+  first_name: "Vika",
+  last_name: "Amir",
+  hourly_rate: 28
+)
